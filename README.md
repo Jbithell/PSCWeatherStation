88b20961f9bf466f6ee75f92fab6ef0868300df5
James Bithell <james@bithell.com>
2020-03-23 19:33:19 +0000

    Remove server function and rely only on pusher data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # PSCWeather
-Uses a live socket connection to Pusher.com to show live weather for Porthmadog (with initial data sent from server)
+Uses a live socket connection to Pusher.com to show live weather for Porthmadog 
 
 ![Screenshot](https://raw.githubusercontent.com/Jbithell/PSCWeatherStation/master/assets/img/github/screenshot1.PNG)
--- assets/main.js
+++ assets/main.js
@@ -22,58 +22,6 @@ var channel = pusher.subscribe('PSCWeatherDataLive');
 //Initialize some vars
 var gauges = [];
 
-//Download chart data from the server - so that earlier data is available.
-function pullServerData() {
-    $.ajax({
-        url: updateurl, success: function (response) {
-            if (response.success) {
-                if (response.message.closed == true) {
-                    //Maintenence closure of weather station - although it can be used for any time of year really
-                    loadingdialog.modal('hide');
-                    $(".datadisplay").hide();
-                    $("#stationcloseddialogue").show();
-                    $("#stationclosedmessage").html(response.message.message);
-                    return false;
-                } else {
-                    $("#stationcloseddialogue").hide();
-
-                    timedifference = response["sent-time"] - response.message["timestamp"]; //timedifference gives you a number in seconds (it's server-server relative so should be acurate) of how upto date the data you're getting is
-                    if ((timedifference/60) > minutesbeforefail) {
-                        //Over 1 hour out of date
-                        $(".datadisplay").hide();
-
-                        $("#nodata").html('<h1>Weather Station Offline</h1><p class="text-center">We have not received data from the Weather Station for ' + Math.round(timedifference/3600) + ' hour' + ((timedifference/3600) != 1 ? 's' : '') + '.<br/>It could be offline for maintenance, or there could be a problem with the internet connection to the club.<br/><button type="button" class="btn btn-default"><a href="https://jbithell.freshdesk.com/support/solutions/articles/42000001330-weather-station-offline">Learn more</a></button></p>');
-                        $("#nodata").fadeIn();
-                    } else if ((timedifference/60) > minutesbeforewarn) {
-                        //5 minutes or more (upto 1 hour) out of date
-                        $("#outofdatedata").html('<strong>Warning</strong> Data is ' + Math.round(timedifference/60) + ' minute' + ((timedifference/60) != 1 ? 's' : '') + ' out of date');
-                        $("#outofdatedata").fadeIn();
-                        $(".datadisplay").show();
-                    } else {
-                        $("#outofdatedata").fadeOut();
-                        $(".datadisplay").show();
-                    }
-                    displayData(response.message);
-                    loadingdialog.modal('hide');
-                }
-            }
-            //How to handle new live Readings from this starting point onwards
-            channel.bind('PSCWeatherDataLiveNEWReading', function(data) {
-                $("#outofdatedata").hide();
-                $("#nodata").hide();
-                displayData(data.message.reading);
-                $(".datadisplay").show();
-            });
-        }, error: function (jqXHR, exception) {
-            //If can't connect to internet/server show a modal explaining that
-            nointernetdialog = bootbox.dialog({
-                message: '<p class="text-center">Error - could not download data updates - please check your internet connection or try again later<br/><br/><i>If this error persists please contact James Bithell using the details at <a href="https://www.jbithell.com" target="_blank">https://www.jbithell.com</a></i><br/><br/></p>',
-                closeButton: false
-            });
-        },
-        cache: false
-    });
-}
 
 // On boot functions
 $( document ).ready(function() {
@@ -83,10 +31,16 @@ $( document ).ready(function() {
     }
     loadingdialog = bootbox.dialog({
         size: "small",
-        message: '<p class="text-center"><i class="fa fa-spinner fa-5x fa-pulse"></i></p>',
+        message: '<p class="text-center"><i class="fa fa-spinner fa-5x fa-pulse"></i><br/><br/><b>Awaiting Data</b><br/>If this message doesn\'t dissapear after a few seconds it\'s likely the station is offline for maintenance such as over the winter period or it has lost its internet connection</p>',
         closeButton: false,
     });
     drawCharts(function () {
-        pullServerData(); //When the charts have been drawn, pull the latest data available from server
+        //How to handle new live Readings from this starting point onwards
+        channel.bind('PSCWeatherDataLiveNEWReading', function(data) {
+            loadingdialog.modal('hide');
+            $(".datadisplay").show();
+            displayData(data.message.reading);
+            $(".datadisplay").show();
+        });
     });
 });
